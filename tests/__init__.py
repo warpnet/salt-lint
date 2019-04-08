e54388b37689b3390b8ec0da809bca5334926cdf
Roald Nefs <info@roaldnefs.com>
2019-04-08 21:04:00 +0200

    Add initial test
    Add initial test, Travis and tox configuration.
    
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2013-2018 Will Thames <will@thames.id.au>
+# Copyright (c) 2018 Ansible by Red Hat
+# Modified work Copyright (c) 2019 Roald Nefs
+
+import tempfile
+
+from saltlint import Runner
+
+
+class RunFromText(object):
+    """Use Runner on temporary files created from unittest text snippets."""
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    def _call_runner(self, path):
+        runner = Runner(self.collection, path, [], [], [])
+        return runner.run()
+
+    def run_state(self, state_text):
+        with tempfile.NamedTemporaryFile() as fp:
+            fp.write(state_text.encode())
+            fp.seek(0)
+            results = self._call_runner(fp.name)
+        return results
---
+++ tests/unit/TestLineToLong.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2013-2018 Will Thames <will@thames.id.au>
+# Copyright (c) 2018 Ansible by Red Hat
+# Modified work Copyright (c) 2019 Roald Nefs
+
+import unittest
+
+from saltlint import RulesCollection
+from saltlint.rules.LineTooLongRule import LineTooLongRule
+from tests import RunFromText
+
+LONG_LINE = '''
+/tmp/testfile:
+  file.managed:
+    - source:/salt://lorem/ipsum/dolor/sit/amet/consectetur/adipiscing/elit/sed/do/eiusmod/tempor/incididunt/ut/labore/et/dolore/magna/aliqua/ut/enim/ad/minim/veniam
+'''
+
+
+class TestLineTooLongRule(unittest.TestCase):
+    collection = RulesCollection()
+    collection.register(LineTooLongRule())
+
+    def setUp(self):
+        self.runner = RunFromText(self.collection)
+
+    def test_long_line(self):
+        results = self.runner.run_state(LONG_LINE)
+        self.assertEqual(1, len(results))

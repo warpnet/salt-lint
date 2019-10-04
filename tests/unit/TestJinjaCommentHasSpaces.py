f2d02536be95d3403a4246adb91abfe5385fbad2
Imran Iqbal <iqbalmy@hotmail.com>
2019-10-04 21:03:23 +0100

    feat(jinja_comments): lint in the same way as statements and variables
    * Related to #13
    * https://jinja.palletsprojects.com/en/2.10.x/templates/#whitespace-control
      - If you add a minus sign (-) to the start or end of a block
        (e.g. a For tag), a comment, or a variable expression,
        the whitespaces before or after that block will be removed

---
+++ tests/unit/TestJinjaCommentHasSpaces.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2013-2018 Will Thames <will@thames.id.au>
+# Copyright (c) 2018 Ansible by Red Hat
+# Modified work Copyright (c) 2019 Roald Nefs
+
+import unittest
+
+from saltlint import RulesCollection
+from saltlint.rules.JinjaCommentHasSpacesRule import JinjaCommentHasSpacesRule
+from tests import RunFromText
+
+
+GOOD_COMMENT_LINE = '''
+{#- set example='good' -#}
+'''
+
+BAD_COMMENT_LINE = '''
+{#-set example='bad'-#}
+'''
+
+class TestLineTooLongRule(unittest.TestCase):
+    collection = RulesCollection()
+
+    def setUp(self):
+        self.collection.register(JinjaCommentHasSpacesRule())
+        self.runner = RunFromText(self.collection)
+
+    def test_comment_positive(self):
+        results = self.runner.run_state(GOOD_COMMENT_LINE)
+        self.assertEqual(0, len(results))
+
+    def test_comment_negative(self):
+        results = self.runner.run_state(BAD_COMMENT_LINE)
+        self.assertEqual(1, len(results))

05766574f0ed84db90374a172bc95a6ec3b87b33
Jeffrey Bouter <jb@warpnet.nl>
2019-10-19 15:20:47 +0200

    Update setup.py
    - Read from README.md instead of rst
    
    Signed-off-by: Jeffrey Bouter <jb@warpnet.nl>

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ from saltlint import (__author__, __license__,
                       NAME, VERSION, DESCRIPTION)
 
 
-# Read long description from the README.rst file
-with io.open('README.rst', 'r', encoding='utf-8') as readme_file:
+# Read long description from the README.md file
+with io.open('README.md', 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 

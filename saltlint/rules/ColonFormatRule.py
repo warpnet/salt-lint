dd563658e22e151c02c84c63919c83cad967fd73
Dawid Malinowski <dawidmalina@gmail.com>
2019-11-15 16:58:34 +0100

    Add rule for colons similar to `yamllint`

---
+++ saltlint/rules/ColonFormatRule.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Dawid Malinowski <dawidmalina@gmail.com>
+
+from saltlint.linter import SaltLintRule
+import re
+
+
+class ColonFormatRule(SaltLintRule):
+    id = '214'
+    shortdesc = 'The line should have an proper colons formatting.'
+    description = 'The line should have an proper colons formatting.'
+    severity = 'MEDIUM'
+    tags = ['formatting']
+    version_added = 'v0.1.1'
+
+    regex = re.compile(r"([\s]{1,}:)|(:\{)|(:\[)")
+
+    def match(self, file, line):
+        return self.regex.search(line)

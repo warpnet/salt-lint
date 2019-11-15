de0f82193adf8c7ebd148d15a4e37bc5e4221545
Dawid Malinowski <dawidmalina@gmail.com>
2019-11-15 22:45:31 +0100

    Add rule for new_line_at_end_of_file similar to `yamllint`

---
+++ saltlint/rules/NewLineAtTheEndRule.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Dawid Malinowski <dawidmalina@gmail.com>
+
+from saltlint.linter import SaltLintRule
+
+class NewLineAtTheEndRule(SaltLintRule):
+    id = '218'
+    shortdesc = 'The POSIX standard requires the last line to be a new line character.'
+    description = 'The POSIX standard requires the last line to be a new line character.'
+    severity = 'LOW'
+    tags = ['formatting']
+    version_added = 'v0.1.1'
+
+    def matchtext(self, file, text):
+        lines = text.splitlines()
+        results = []
+        if lines and '' != lines[-1]:
+            results.append((len(lines), lines[-1], None))
+
+        return results

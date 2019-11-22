a586dc4481fe91b272b2149545d7812f01373b17
Mario Trangoni <mjtrangoni@gmail.com>
2019-11-22 15:25:17 +0100

    Fix pylint `wrong-import-order` issue
    See,
    wrong-import-order (C0411): Used when PEP8 import order is not respected (standard imports first, then third-party libraries, then local imports)
    
    Signed-off-by: Mario Trangoni <mjtrangoni@gmail.com>

--- saltlint/rules/JinjaStatementHasSpacesRule.py
+++ saltlint/rules/JinjaStatementHasSpacesRule.py
@@ -3,8 +3,8 @@
 # Copyright (c) 2018 Ansible Project
 # Modified work Copyright (c) 2019 Warpnet B.V.
 
-from saltlint.linter import SaltLintRule
 import re
+from saltlint.linter import SaltLintRule
 
 
 class JinjaStatementHasSpacesRule(SaltLintRule):

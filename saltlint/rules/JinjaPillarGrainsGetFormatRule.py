a3454920b54553df3a9b5ef5a7da753822f90738
Roald Nefs <info@roaldnefs.com>
2020-04-16 07:58:46 +0200

    Fix rule 211
    Fix rule `211`. The rule should have spotted the usage of `pillar.get['key']` or `grains.get['key']`. All example on the [wiki](https://github.com/warpnet/salt-lint/wiki/211) are actually correct examples and will be rendered in Jinja without any problems.
    
    Using `pillar.get['key']` in a SLS file results in:
    
    ```
    local:
        Data failed to compile:
    ----------
        Rendering SLS 'base:example' failed: Jinja variable 'method object' has no attribute ‘key'
    ```
    
    Using `grains.get['key']` results in:
    ```
    local:
        Data failed to compile:
    ----------
        Rendering SLS 'base:example' failed: Jinja variable 'builtin_function_or_method object' has no attribute 'key'
    ```
    
    Fixes #140
    
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- saltlint/rules/JinjaPillarGrainsGetFormatRule.py
+++ saltlint/rules/JinjaPillarGrainsGetFormatRule.py
@@ -10,13 +10,14 @@ from saltlint.linter import SaltLintRule
 class JinjaPillarGrainsGetFormatRule(SaltLintRule):
     id = '211'
     shortdesc = 'pillar.get or grains.get should be formatted differently'
-    description = "pillar.get and grains.get should always be formatted" \
-                  " like salt['pillar.get']('item') or grains['item1']"
+    description = "pillar.get and grains.get should always be formatted " \
+                  "like salt['pillar.get']('item'), grains['item1'] or " \
+                  " pillar.get('item')"
     severity = 'HIGH'
     tags = ['formatting', 'jinja']
     version_added = 'v0.0.10'
 
-    bracket_regex = re.compile(r"{{( |\-|\+)?.(pillar|grains).get\(.+}}")
+    bracket_regex = re.compile(r"{{( |\-|\+)?.(pillar|grains).get\[.+}}")
 
     def match(self, _, line):
         return self.bracket_regex.search(line)

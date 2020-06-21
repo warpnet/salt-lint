9e6490008b93a54ff25180ca6507540d5a775946
Roald Nefs <info@roaldnefs.com>
2020-06-21 10:32:53 +0200

    fix: remove salt as a dependency
    Remove Salt as a dependency of salt-lint by:
    - Including the `six` and `pyyaml` libraries directly.
    - Specify the colors directly.
    
    This also fixes #135.
    
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- saltlint/formatters/__init__.py
+++ saltlint/formatters/__init__.py
@@ -5,11 +5,36 @@
 
 import json
 
-# Import salt libs
-try:
-    import salt.utils.color as saltcolor
-except ImportError:
-    import salt.utils as saltcolor
+
+def get_colors(use=True):
+    """
+    Return the colors as a dict, pass False to return the colors as empty
+    strings.
+    """
+    colors = {
+        "BLACK": "\033[0;30m", # black
+        "DARK_GRAY": "\033[1;30m", # bold, black
+        "RED": "\033[0;31m",# red
+        "LIGHT_RED": "\033[1;31m" , # bold, red
+        "GREEN": "\033[0;32m", # green
+        "LIGHT_GREEN": "\033[1;32m", # bold, green
+        "BLUE": "\033[0;34m", # blue
+        "LIGHT_BLUE": "\033[1;34m", # bold, blue
+        "MAGENTA": "\033[0;35m", # magenta,
+        "LIGHT_MAGENTA": "\033[1;35m", # bold, magenta
+        "CYAN": "\033[0;36m", # cyan
+        "LIGHT_CYAN": "\033[1;36m", # bold, cyan
+        "LIGHT_GRAY": "\033[0;37m", # white
+        "WHITE": "\033[1;37m", # bold, white
+        "DEFAULT_COLOR": "\033[00m", # default
+        "ENDC": "\033[0m", # reset
+    }
+
+    if not use:
+        for color in colors:
+            colors[color] = ''
+
+    return colors
 
 
 class Formatter(object):
@@ -20,26 +45,19 @@ class Formatter(object):
 
     def format(self, match, colored=False):
         formatstr = u"{0} {1}\n{2}:{3}\n{4}\n"
-        if colored:
-            color = saltcolor.get_colors()
-            return formatstr.format(
-                u'{0}[{1}]{2}'.format(color['RED'], match.rule.id,
-                                      color['ENDC']),
-                u'{0}{1}{2}'.format(color['LIGHT_RED'], match.message,
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['BLUE'], match.filename,
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['CYAN'], str(match.linenumber),
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['MAGENTA'], match.line, color['ENDC'])
-            )
-        else:
-            return formatstr.format(
-                u'[{0}]'.format(match.rule.id),
-                match.message,
-                match.filename,
-                match.linenumber,
-                match.line)
+
+        color = get_colors(self.colored)
+        return formatstr.format(
+            u'{0}[{1}]{2}'.format(color['RED'], match.rule.id,
+                                  color['ENDC']),
+            u'{0}{1}{2}'.format(color['LIGHT_RED'], match.message,
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['BLUE'], match.filename,
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['CYAN'], str(match.linenumber),
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['MAGENTA'], match.line, color['ENDC'])
+        )
 
 
 class SeverityFormatter(object):
@@ -50,29 +68,20 @@ class SeverityFormatter(object):
     def format(self, match, colored=False):
         formatstr = u"{0} {sev} {1}\n{2}:{3}\n{4}\n"
 
-        if colored:
-            color = saltcolor.get_colors()
-            return formatstr.format(
-                u'{0}[{1}]{2}'.format(color['RED'], match.rule.id,
-                                      color['ENDC']),
-                u'{0}{1}{2}'.format(color['LIGHT_RED'], match.message,
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['BLUE'], match.filename,
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['CYAN'], str(match.linenumber),
-                                    color['ENDC']),
-                u'{0}{1}{2}'.format(color['MAGENTA'], match.line, color['ENDC']),
-                sev=u'{0}[{1}]{2}'.format(color['RED'], match.rule.severity,
-                                          color['ENDC'])
-            )
-        else:
-            return formatstr.format(
-                u'[{0}]'.format(match.rule.id),
-                match.message,
-                match.filename,
-                match.linenumber,
-                match.line,
-                sev=u'[{0}]'.format(match.rule.severity))
+        color = get_colors(self.colored)
+        return formatstr.format(
+            u'{0}[{1}]{2}'.format(color['RED'], match.rule.id,
+                                  color['ENDC']),
+            u'{0}{1}{2}'.format(color['LIGHT_RED'], match.message,
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['BLUE'], match.filename,
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['CYAN'], str(match.linenumber),
+                                color['ENDC']),
+            u'{0}{1}{2}'.format(color['MAGENTA'], match.line, color['ENDC']),
+            sev=u'{0}[{1}]{2}'.format(color['RED'], match.rule.severity,
+                                      color['ENDC'])
+        )
 
 
 class JsonFormatter(object):

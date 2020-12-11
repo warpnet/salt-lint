b89f92f5b2c8050e53e35f155fb9a585f8fa3037
Roald Nefs <info@roaldnefs.com>
2020-12-11 14:54:16 +0100

    fix: yaml indentation for rule 215
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- saltlint/rules/FileManagedReplaceContentRule.py
+++ saltlint/rules/FileManagedReplaceContentRule.py
@@ -18,10 +18,10 @@ class FileManagedReplaceContentRule(Rule):
     version_added = 'develop'
 
     # Find the full file.managed state
-    regex = re.compile(r"^\s{2}file\.managed:.*(?:\n\s{4}.+)*", re.MULTILINE)
+    regex = re.compile(r"^\s{2}file\.managed:.*(?:\n(?:\s{4}|\s{2}-).+)*", re.MULTILINE)
     # Regex for finding the content source option
     regex_options= re.compile(
-        r"^\s{4}-\s(?:source:|contents:|contents_pillar:|contents_grains:|replace:\s[F|f]alse).*$",
+        r"^(?:\s{2}|\s{4})-\s(?:source:|contents:|contents_pillar:|contents_grains:|replace:\s[F|f]alse).*$",
         re.MULTILINE
     )
 

0fd9a0d4dc2057d061af91ed1b7c4c7a94ba6766
Roald Nefs <info@roaldnefs.com>
2021-05-31 21:37:30 +0200

    feat: simplify typographical error rules
    Simplify the typographical error rules by creating a base
    `TypographicalErrorRule` that can be extended by a simple regex. This
    commit also adds the missing `CHANGELOG.md` entry.
    
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- saltlint/rules/TypoContentsRule.py
+++ saltlint/rules/TypoContentsRule.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2016 Will Thames and contributors
 # Copyright (c) 2018 Ansible Project
-# Modified work Copyright (c) 2020 Warpnet B.V.
-# Copyright (c) 2021 Yury Bushmelev
+# Modified work Copyright (c) 2021 Warpnet B.V.
+# Modified work Copyright (c) 2021 Yury Bushmelev
 
 import re
-from saltlint.linter.rule import Rule
-from saltlint.utils import LANGUAGE_SLS
+from saltlint.linter.rule import TypographicalErrorRule
 
 
-class TypoContentsRule(Rule):
+class TypoContentsRule(TypographicalErrorRule):
     id = '218'
     shortdesc = '"content" looks like a typo. Did you mean "contents"?'
     description = '"content" looks like a typo. Did you mean "contents"?'
-    severity = 'HIGH'
-    languages = [LANGUAGE_SLS]
-    tags = ['formatting']
-    version_added = 'v0.5.3'
-
+    version_added = 'develop'
     regex = re.compile(r"^\s+- content(|_pillar|_grains|_newline|_delimiter):")
-
-    def match(self, file, line):
-        return self.regex.search(line)
--- saltlint/rules/TypoRequireRule.py
+++ saltlint/rules/TypoRequireRule.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2016 Will Thames and contributors
 # Copyright (c) 2018 Ansible Project
-# Modified work Copyright (c) 2020 Warpnet B.V.
-# Copyright (c) 2021 Yury Bushmelev
+# Modified work Copyright (c) 2021 Warpnet B.V.
+# Modified work Copyright (c) 2021 Yury Bushmelev
 
 import re
-from saltlint.linter.rule import Rule
-from saltlint.utils import LANGUAGE_SLS
+from saltlint.linter.rule import TypographicalErrorRule
 
 
-class TypoRequireRule(Rule):
+class TypoRequireRule(TypographicalErrorRule):
     id = '217'
     shortdesc = '"requires" looks like a typo. Did you mean "require"?'
     description = '"requires" looks like a typo. Did you mean "require"?'
-    severity = 'HIGH'
-    languages = [LANGUAGE_SLS]
-    tags = ['formatting']
-    version_added = 'v0.5.3'
-
+    version_added = 'develop'
     regex = re.compile(r"^\s+- requires(|_in|_any):")
-
-    def match(self, file, line):
-        return self.regex.search(line)
